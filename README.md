16c51d25b3246b50005fb1b5020f8684017fcfeb
Kallirroi <kallirroi.retzepi@gmail.com>
2020-07-29 09:50:21 -0400

    fetching correctly, added react-use for Hooks

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## OpenWeather CRA app
 
 A CRA (create-react-app) frontend that allows the user to enter a location and be presented with the weather there, which uses the [Ant Design](https://ant.design/docs/react/introduce) system.
+
+
+### Process
+OpenWeather is pretty easy to sign up for, and all membership options use ["One Call API"](https://openweathermap.org/api/one-call-api) which looks like an updated API that bundles current, minute, hourly, daily and some historical data all in one. Great!
--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, {useState }from 'react';
 import './App.css';
 
+import { Typography, Space, Button } from 'antd';
+
+const { Text, Link, Title } = Typography;
+
 function App() {
+
+  const api = {
+    key: `f87be23187f2caeeb062a2c01d2c85bd`,
+    base: `https://api.openweathermap.org/data/2.5/onecall`
+  }
+
+  const coordinates = { //dummy coordinates for now
+    lat: `40.12`,
+    long: `-96.66`
+  }
+
+  const [request, setRequest] = useState('');
+  const [results, setResults] = useState({});
+
+  function handleClick(e) {
+    e.preventDefault()
+    console.log('fetching')
+    fetch(`${api.base}?lat=${coordinates.lat}&lon=${coordinates.long}&appid=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.log(error))
+  }
+
   return (
     <div className="App">
-      hey!
+        <Title>OpenWeather app</Title>
+        
+        <Space direction="vertical">
+          <Text>A simple weather app that geolocates you and fetches the weather forecast using the <Link href="https://openweathermap.org/api/one-call-api" target="_blank">OpenWeather API</Link></Text>
+        </Space>
+
+        <Space direction="vertical">
+          <Button 
+            type="primary"
+            onClick={handleClick}
+            >
+            Fetch dummy data
+          </Button>
+        </Space>
     </div>
   );
 }
